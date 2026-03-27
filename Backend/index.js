e0c54b82286002288ee8c03d28428a1de4009f92
Prithivi karki <prithvikarki123@gmail.com>
2026-03-27 19:23:50 +0530

    Demo for gemini API call

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import "dotenv/config";
+import { GoogleGenAI } from "@google/genai";
 
 const app = express();
 
@@ -9,3 +10,15 @@ app.use(express.urlencoded());
 app.listen(process.env.PORT || 3000, (req, res) => {
   console.log(`Server started at ${process.env.PORT}`);
 });
+
+try {
+  const ai = new GoogleGenAI({});
+  const response = await ai.models.generateContent({
+    model: "gemini-3-flash-preview",
+    contents: "1+1",
+  });
+
+  console.log(response.text);
+} catch (error) {
+  console.log(error);
+}
